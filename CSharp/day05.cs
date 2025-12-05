6a34729ab37e0c25acfa0a5efb595cb830965451
Matthias <matthias@schmidt-dot.net>
2025-12-05 13:54:08 +0100

    solved day 5 in C#

---
+++ CSharp/day05.cs
@@ -0,0 +1,116 @@
+namespace AdventOfCode2025;
+
+using FluentAssertions;
+using NUnit.Framework;
+
+using matthiasffm.Common.Collections;
+using matthiasffm.Common.Math;
+
+/// <summary>
+/// Theme: merge id ranges
+/// </summary>
+[TestFixture]
+public class Day05
+{
+    private static ((long start, long end)[] freshIngredients, long[] availableIngredients) ParseData(string[] data)
+    {
+        var split = string.Join('#', data)
+                          .Split("##", StringSplitOptions.RemoveEmptyEntries);
+        return (split[0].Split('#').Select(line => (long.Parse(line.Split('-')[0]), long.Parse(line.Split('-')[1]))).ToArray(), 
+                split[1].Split('#').Select(line => long.Parse(line)).ToArray());
+    }
+
+    [Test]
+    public void TestSamples()
+    {
+        var data = new[]
+        {
+            "3-5",
+            "10-14",
+            "16-20",
+            "12-18",
+            "",
+            "1",
+            "5",
+            "8",
+            "11",
+            "17",
+            "32",
+        };
+        var (freshIngredients, availableIngredients) = ParseData(data);
+
+        Puzzle1(freshIngredients, availableIngredients).Should().Be(3L);
+        Puzzle2(freshIngredients).Should().Be(14L);
+    }
+
+    [Test]
+    public void TestAocInput()
+    {
+        var data = FileUtils.ReadAllLines(this);
+        var (freshIngredients, availableIngredients) = ParseData(data);
+
+        Puzzle1(freshIngredients, availableIngredients).Should().Be(511L);
+        Puzzle2(freshIngredients).Should().Be(350939902751909L);
+    }
+
+    // The Elves can't figure out which of their ingredients are fresh and which are spoiled. When you ask how it works, they give you a copy
+    // of their database (your puzzle input). The database operates on ingredient IDs. It consists of a list of fresh ingredient ID ranges, a
+    // blank line, and a list of available ingredient IDs. The fresh ID ranges are inclusive and can also overlap; an ingredient ID is fresh
+    // if it is in any range.
+    //
+    // Puzzle == Process the database file from the new inventory management system. How many of the available ingredient IDs are fresh?
+    private static long Puzzle1((long start, long end)[] freshIngredients, long[] availableIngredients)
+        => availableIngredients.Sum(ingredient => freshIngredients.Any(fresh => ingredient.Between(fresh.start, fresh.end)) ? 1L : 0L);
+
+    // The Elves would also like to know all of the IDs that the fresh ingredient ID ranges consider to be fresh. An ingredient ID is still
+    // considered fresh if it is in any range.
+    //
+    // Puzzle == Process the database file again. How many ingredient IDs are considered to be fresh according to the fresh ingredient ID ranges?
+    private static long Puzzle2(IEnumerable<(long start, long end)> freshIngredients)
+    {
+        // merge ranges
+
+        bool merged;
+        do
+        {
+            merged = false;
+
+            // worse than O(n²)?
+            // faster => sort ranges by start and insert them in a linked list
+            //           every iteration try to merge two adjacent items in the list
+            //           nothing to merge anymore => end
+            //           (and if start1 < start2 is guaranteed intersect is when end1 > start2)
+
+            var mergedRanges = new HashSet<(long start, long end)>(freshIngredients);
+
+            foreach(var pair in freshIngredients.Variations())
+            {
+                if(RangesIntersect(pair.Item1, pair.Item2))
+                {
+                    var mergedRange = MergeRanges(pair.Item1, pair.Item2);
+                    mergedRanges.Remove(pair.Item1);
+                    mergedRanges.Remove(pair.Item2);
+                    mergedRanges.Add(mergedRange);
+
+                    merged = true;
+
+                    break;
+                }
+            }
+
+            freshIngredients = mergedRanges;
+        }
+        while(merged == true);
+
+        // sum merged range sizes
+
+        return freshIngredients.Sum(range => range.end - range.start + 1);
+    }
+
+    private static bool RangesIntersect((long start, long end) left, (long start, long end) right)
+        => left.start.Between(right.start, right.end) ||
+           left.end.Between(right.start, right.end);
+
+    private static (long start, long end) MergeRanges((long start, long end) left, (long start, long end) right)
+        => (Math.Min(left.start, right.start), Math.Max(left.end, right.end));
+}
